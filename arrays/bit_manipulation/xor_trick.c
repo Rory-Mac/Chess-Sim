0c63fdebce6add1efde02a79eef78d70f5b4ed7b
Rory Maclean <roryobm@outlook.com>
2023-11-03 22:40:38 +1100

    XOR trick, XOR values 1 to n with bit manipulation

--- arrays/bit_manipulation/xor_trick.c
+++ arrays/bit_manipulation/xor_trick.c
@@ -1,54 +1,51 @@
-int XOR_trick(n) {
+/*
+x ^ (x + 1) = 1 (if x is even)
+
+0^1^2^3^4^5^6^7
+(0^1)^(2^3)^(4^5)^(6^7)
+1^1^1^1
+0
+
+0^1^2^3^4^5^6^7^n (n mod 4 == 0)
+0^1^2^3^4^5^6^7^8^n (n mod 4 == 1)
+0^1^2^3^4^5^6^7^8^9^n (n mod 4 == 2)
+0^1^2^3^4^5^6^7^8^9^10^n (n mod 4 == 3)
+
+0^n = n
+0^1 = 1
+0^1^n = n + 1 
+0^1^1 = 0
+
+*/
+#include <stdio.h>
+
+int XOR_trick(int n) {
     int x = n % 4;
     if (x == 0) {
         return n;
     } else if (x == 1) {
-
+        return 1;
     } else if (x == 2) {
-
+        return n + 1;
     } else if (x == 3) {
-
+        return 0;
     }
+}
 
+int naive_approach(int n) {
+    int result = 0;
+    for (int i = 0; i <= n; i++) {
+        result ^= i;
+    }
+    return result;
 }
 
 int main() {
     int n;
-    scanf("%d", &n);
-    int result = XOR_trick(n);
-    printf("Result: %d", result);
+    while (1) {
+        printf("Enter number: ");
+        scanf("%d", &n);
+        printf("%d %d\n", XOR_trick(n), naive_approach(n));
+    }
     return 0;
 }
-
-/*
-a^0 = a
-a^1 = !a
-a^a = 0
-
-a^b^c^d^a^b^c^d^a^b^c
-a^a^b^b^c^c^d^d^a^b^c
-a^b^c
-
-1^2^3^4^5^6^7^8
-(1^5)^(2^6)^(3^7)^(4^8)
-
-x % 4 == 1
-0001
-0101
-1001
-1101
-x % 4 == 2
-0010
-0110
-1010
-1110
-x % 4 == 3
-0011
-0111
-1011
-1111
-x % 4 == 0
-0100
-1000
-1010
-*/
