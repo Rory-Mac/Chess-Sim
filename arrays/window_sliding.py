584c9151331d41a2309e1276381f0204a6002219
Rory Maclean <roryobm@outlook.com>
2023-11-11 23:21:20 +1100

    add pythonic equivalent to sliding window exercise

--- arrays/window_sliding.py
+++ arrays/window_sliding.py
@@ -1,3 +1,12 @@
+# equivalent function but more pythonic
+def max_consecutive_sum_reduced(items, k):
+    current_sum = sum(items[:k])
+    max_sum = current_sum
+    for i in range(len(items) - k):
+        current_sum = current_sum - items[i] + items[i + k]
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+
 # return the maximum sum of k consecutive elements in the given array
 def max_consecutive_sum(items, k):
     lo = 0
@@ -20,3 +29,5 @@ if __name__ == "__main__":
     items = [1, 4, 2, 10, 23, 3, 1, 0, 20]
     assert max_consecutive_sum(items, 4) == 39
     assert max_consecutive_sum(items, 5) == 47
+    assert max_consecutive_sum_reduced(items, 4) == 39
+    assert max_consecutive_sum_reduced(items, 5) == 47
