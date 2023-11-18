900e673a77fab649448ca79d220a8b03e67a9304
Rory Maclean <roryobm@outlook.com>
2023-11-18 19:01:19 +1100

    product array except self (solve with constant space complexity)

--- neet/blind75/arrays_hashing/productArrayExceptSelf.py
+++ neet/blind75/arrays_hashing/productArrayExceptSelf.py
@@ -1,4 +1,5 @@
 class ProductofArrayExceptSelf(object):
+    # O(n) time complexity + O(n) space complexity
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
@@ -20,4 +21,22 @@ class ProductofArrayExceptSelf(object):
                 answer[i] = prefix_product[i - 1]
             else:
                 answer[i] = prefix_product[i - 1] * suffix_product[i + 1]
+        return answer
+    
+    # O(n) time complexity + O(1) space complexity (excluding result array)
+    def productExceptSelfAlternative(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = [0] * len(nums)
+        answer[-1] = nums[-1]
+        for i in range(2, len(nums) + 1):
+            answer[-i] = nums[-i] * answer[-i + 1]
+        answer[0] = answer[1]
+        prefix_product = nums[0]
+        for i in range(1, len(nums) - 1):
+            answer[i] = prefix_product * answer[i + 1]
+            prefix_product *= nums[i]
+        answer[-1] = prefix_product
         return answer
