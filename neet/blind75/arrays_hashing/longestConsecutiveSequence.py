f6d9128587cd7712f24863ba4b3dd7a1958f5d2c
Rory Maclean <roryobm@outlook.com>
2023-11-19 01:15:12 +1100

    O(n) complete solution - longest consecutive sequence (blind 75)

--- neet/blind75/arrays_hashing/longestConsecutiveSequence.py
+++ neet/blind75/arrays_hashing/longestConsecutiveSequence.py
@@ -6,35 +6,12 @@ class LongestConsecutiveSequence(object):
         :type nums: List[int]
         :rtype: int
         """
-        # [0,1,2,3,4,5,6,7,8,9]
-        # [5,6,7,8,9,0,1,2,3,4]
-        # [100,4,200,1,3,2]
-        # [9,8,7,6,5,4,3,2,1]
-
-        items = {num : num for num in nums}
-        searched = {}
+        items = set(nums)
         max_count = 0
-        for num in nums:
-            if searched.get(num, None) != None: continue
-            successor = num
-            consecutive_count = 1
-            while True:
-                successor += 1
-                if items.get(successor, None) == None: break
-                consecutive_count += 1
-                if searched.get(successor, None) != None:
-                    consecutive_count_tail = searched.pop(successor)
-                    consecutive_count += consecutive_count_tail - 1
-                    break
-                else:
-                    searched[successor] = successor
-            searched[num] = consecutive_count
-            if consecutive_count > max_count: max_count = consecutive_count
+        for x in nums:
+            if x - 1 not in items:
+                y = x + 1
+                while y in items:
+                    y += 1
+                max_count = max(max_count, y - x)
         return max_count
-
-
-if __name__ == "__main__":
-    nums = [100,4,200,1,3,2]
-    instance = LongestConsecutiveSequence()
-    result = instance.longestConsecutive(nums)
-    print(result)
