44d996038f63b124c245ed5977fd02ed41379c08
Rory Maclean <roryobm@outlook.com>
2023-11-22 14:51:38 +1100

    binary search refresher

---
+++ neet/blind75/binary_search/binary_search.py
@@ -0,0 +1,18 @@
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+# If target exists, then return its index. Otherwise, return -1.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (r + l) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+        return -1
