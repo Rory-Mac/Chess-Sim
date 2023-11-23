75995165a5be6a04ab0892f5cb72e9dfee1810a4
Rory Maclean <roryobm@outlook.com>
2023-11-24 00:20:11 +1100

    render initial game board + pieces

--- miscellaneous/board_games/chess/game.py
+++ miscellaneous/board_games/chess/game.py
@@ -10,28 +10,55 @@ pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
 # load game assets
 king_black = pygame.image.load(os.getcwd() + '/assets/king_black.png')
+king_black = pygame.transform.scale(king_black, (100, 100))
 king_white = pygame.image.load(os.getcwd() + '/assets/king_white.png')
+king_white = pygame.transform.scale(king_white, (100, 100))
 queen_black = pygame.image.load(os.getcwd() + '/assets/queen_black.png')
+queen_black = pygame.transform.scale(queen_black, (100, 100))
 queen_white = pygame.image.load(os.getcwd() + '/assets/queen_white.png')
+queen_white = pygame.transform.scale(queen_white, (100, 100))
 rook_black = pygame.image.load(os.getcwd() + '/assets/rook_black.png')
+rook_black = pygame.transform.scale(rook_black, (100, 100))
 rook_white = pygame.image.load(os.getcwd() + '/assets/rook_white.png')
+rook_white = pygame.transform.scale(rook_white, (100, 100))
 bishop_black = pygame.image.load(os.getcwd() + '/assets/bishop_black.png')
+bishop_black = pygame.transform.scale(bishop_black, (100, 100))
 bishop_white = pygame.image.load(os.getcwd() + '/assets/bishop_white.png')
+bishop_white = pygame.transform.scale(bishop_white, (100, 100))
 knight_black = pygame.image.load(os.getcwd() + '/assets/knight_black.png')
+knight_black = pygame.transform.scale(knight_black, (100, 100))
 knight_white = pygame.image.load(os.getcwd() + '/assets/knight_white.png')
+knight_white = pygame.transform.scale(knight_white, (100, 100))
 pawn_black = pygame.image.load(os.getcwd() + '/assets/pawn_black.png')
+pawn_black = pygame.transform.scale(pawn_black, (100, 100))
 pawn_white = pygame.image.load(os.getcwd() + '/assets/pawn_white.png')
-# draw chess pieces
-screen.blit(king_black, (0,0))
-screen.blit(king_black, (0,0))
+pawn_white = pygame.transform.scale(pawn_white, (100, 100))
 # draw overlayed board
-#screen.fill(WHITE)
-#for x in range(8):
-#    for y in range(8):
-#        pygame.draw.rect(screen, BLACK, pygame.Rect(100*x + 5, 100*y + 5, 90, 90))
-
-
-
+screen.fill(BLACK)
+for x in range(8):
+    for y in range(8):
+        pygame.draw.rect(screen, WHITE, pygame.Rect(100*x + 5, 100*y + 5, 90, 90))
+# draw chess pieces
+screen.blit(rook_black, (0,0))
+screen.blit(knight_black, (100,0))
+screen.blit(bishop_black, (200,0))
+screen.blit(queen_black, (300,0))
+screen.blit(king_black, (400,0))
+screen.blit(bishop_black, (500,0))
+screen.blit(knight_black, (600,0))
+screen.blit(rook_black, (700,0))
+screen.blit(rook_white, (0,700))
+screen.blit(knight_white, (100,700))
+screen.blit(bishop_white, (200,700))
+screen.blit(queen_white, (300,700))
+screen.blit(king_white, (400,700))
+screen.blit(bishop_white, (500,700))
+screen.blit(knight_white, (600,700))
+screen.blit(rook_white, (700,700))
+for i in range(8):
+    screen.blit(pawn_black, (i*100,100))
+    screen.blit(pawn_white, (i*100,600))
+# main game loop
 running = True
 while running:
     for event in pygame.event.get():
