3e0c6a0fc01ae1000a57c06fd5d5d5ae328697ea
Rory Maclean <roryobm@outlook.com>
2023-11-24 16:24:25 +1100

    chess piece highlighting on-click

--- miscellaneous/board_games/chess/game.py
+++ miscellaneous/board_games/chess/game.py
@@ -1,72 +1,129 @@
 import pygame
 import os
- 
+from game_board import GameBoard, PieceColor, King, Queen, Bishop, Knight, Rook, Pawn
+
+#--------------------------------------------------------------------------------------------------------------
+# Initialisation
+#--------------------------------------------------------------------------------------------------------------
+# game constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 # initialise game
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
-# load game assets
-king_black = pygame.image.load(os.getcwd() + '/assets/king_black.png')
-king_black = pygame.transform.scale(king_black, (100, 100))
-king_white = pygame.image.load(os.getcwd() + '/assets/king_white.png')
-king_white = pygame.transform.scale(king_white, (100, 100))
-queen_black = pygame.image.load(os.getcwd() + '/assets/queen_black.png')
-queen_black = pygame.transform.scale(queen_black, (100, 100))
-queen_white = pygame.image.load(os.getcwd() + '/assets/queen_white.png')
-queen_white = pygame.transform.scale(queen_white, (100, 100))
-rook_black = pygame.image.load(os.getcwd() + '/assets/rook_black.png')
-rook_black = pygame.transform.scale(rook_black, (100, 100))
-rook_white = pygame.image.load(os.getcwd() + '/assets/rook_white.png')
-rook_white = pygame.transform.scale(rook_white, (100, 100))
-bishop_black = pygame.image.load(os.getcwd() + '/assets/bishop_black.png')
-bishop_black = pygame.transform.scale(bishop_black, (100, 100))
-bishop_white = pygame.image.load(os.getcwd() + '/assets/bishop_white.png')
-bishop_white = pygame.transform.scale(bishop_white, (100, 100))
-knight_black = pygame.image.load(os.getcwd() + '/assets/knight_black.png')
-knight_black = pygame.transform.scale(knight_black, (100, 100))
-knight_white = pygame.image.load(os.getcwd() + '/assets/knight_white.png')
-knight_white = pygame.transform.scale(knight_white, (100, 100))
-pawn_black = pygame.image.load(os.getcwd() + '/assets/pawn_black.png')
-pawn_black = pygame.transform.scale(pawn_black, (100, 100))
-pawn_white = pygame.image.load(os.getcwd() + '/assets/pawn_white.png')
-pawn_white = pygame.transform.scale(pawn_white, (100, 100))
+# initialise game components
+board = GameBoard()
+game_orientation = PieceColor.WHITE
+# access game assets
+game_assests = {
+    PieceColor.BLACK : {
+        King : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/king_black.png'), (100, 100)),
+        Queen : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/queen_black.png'), (100, 100)),
+        Rook : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/rook_black.png'), (100, 100)),
+        Bishop : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/bishop_black.png'), (100, 100)),
+        Knight : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/knight_black.png'), (100, 100)),
+        Pawn : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/pawn_black.png'), (100, 100))
+    },
+    PieceColor.WHITE : {
+        King : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/king_white.png'), (100, 100)),
+        Queen : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/queen_white.png'), (100, 100)),
+        Rook : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/rook_white.png'), (100, 100)),
+        Bishop : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/bishop_white.png'), (100, 100)),
+        Knight : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/knight_white.png'), (100, 100)),
+        Pawn : pygame.transform.scale(pygame.image.load(os.getcwd() + '/assets/pawn_white.png'), (100, 100))
+    }
+}
 # draw overlayed board
 screen.fill(BLACK)
 for x in range(8):
     for y in range(8):
         pygame.draw.rect(screen, WHITE, pygame.Rect(100*x + 5, 100*y + 5, 90, 90))
 # draw chess pieces
-screen.blit(rook_black, (0,0))
-screen.blit(knight_black, (100,0))
-screen.blit(bishop_black, (200,0))
-screen.blit(queen_black, (300,0))
-screen.blit(king_black, (400,0))
-screen.blit(bishop_black, (500,0))
-screen.blit(knight_black, (600,0))
-screen.blit(rook_black, (700,0))
-screen.blit(rook_white, (0,700))
-screen.blit(knight_white, (100,700))
-screen.blit(bishop_white, (200,700))
-screen.blit(queen_white, (300,700))
-screen.blit(king_white, (400,700))
-screen.blit(bishop_white, (500,700))
-screen.blit(knight_white, (600,700))
-screen.blit(rook_white, (700,700))
+screen.blit(game_assests[PieceColor.BLACK][Rook], (0,0))
+screen.blit(game_assests[PieceColor.BLACK][Knight], (100,0))
+screen.blit(game_assests[PieceColor.BLACK][Bishop], (200,0))
+screen.blit(game_assests[PieceColor.BLACK][Queen], (300,0))
+screen.blit(game_assests[PieceColor.BLACK][King], (400,0))
+screen.blit(game_assests[PieceColor.BLACK][Bishop], (500,0))
+screen.blit(game_assests[PieceColor.BLACK][Knight], (600,0))
+screen.blit(game_assests[PieceColor.BLACK][Rook], (700,0))
+screen.blit(game_assests[PieceColor.WHITE][Rook], (0,700))
+screen.blit(game_assests[PieceColor.WHITE][Knight], (100,700))
+screen.blit(game_assests[PieceColor.WHITE][Bishop], (200,700))
+screen.blit(game_assests[PieceColor.WHITE][Queen], (300,700))
+screen.blit(game_assests[PieceColor.WHITE][King], (400,700))
+screen.blit(game_assests[PieceColor.WHITE][Bishop], (500,700))
+screen.blit(game_assests[PieceColor.WHITE][Knight], (600,700))
+screen.blit(game_assests[PieceColor.WHITE][Rook], (700,700))
 for i in range(8):
-    screen.blit(pawn_black, (i*100,100))
-    screen.blit(pawn_white, (i*100,600))
-# main game loop
+    screen.blit(game_assests[PieceColor.BLACK][Pawn], (i*100,100))
+    screen.blit(game_assests[PieceColor.WHITE][Pawn], (i*100,600))
+
+#--------------------------------------------------------------------------------------------------------------
+# Helper functions
+#--------------------------------------------------------------------------------------------------------------
+def highlight_tile(tile : (int, int)):
+    piece = board.get_piece(tile)
+    if not piece: return
+    sprite = game_assests[piece.getColor()][piece.__class__]
+    x, y = tile[0], tile[1]
+    pygame.draw.rect(screen, GREEN, pygame.Rect(100*x, 100*y, 100, 100))
+    pygame.draw.rect(screen, WHITE, pygame.Rect(100*x + 5, 100*y + 5, 90, 90))
+    screen.blit(sprite, (100*x,100*y))
+
+def unhighlight_tile(tile : (int, int)):
+    piece = board.get_piece(tile)
+    sprite = game_assests[piece.getColor()][piece.__class__]
+    x, y = tile[0], tile[1]
+    pygame.draw.rect(screen, BLACK, pygame.Rect(100*x, 100*y, 100, 100))
+    pygame.draw.rect(screen, WHITE, pygame.Rect(100*x + 5, 100*y + 5, 90, 90))
+    screen.blit(sprite, (100*x,100*y))
+
+# user clicks on a new tile
+#   if new tile is selected tile
+#       unhighlight
+#   if new tile contains own piece
+#       unhighlight previously selected tile if it exists
+#       highlight and select new tile
+#   if a tile is already selected and new tile does not contain own piece and move is valid
+#       set target tile to none, move piece, unhighlight selected tile
+def process_click_event(click_coord : (int, int)):
+    clicked_tile = (click_coord[0] // 100, click_coord[1] // 100)
+    clicked_piece = board.get_piece(clicked_tile)
+    selected_tile = board.get_selected_tile()
+    if selected_tile == clicked_tile:
+        unhighlight_tile(selected_tile)
+        board.set_selected_tile(None)
+    elif clicked_piece and clicked_piece.getColor() == game_orientation:
+        if selected_tile: unhighlight_tile(selected_tile)
+        board.set_selected_tile(clicked_tile)
+        highlight_tile(clicked_tile)
+    elif selected_tile and board.makeMove(clicked_tile):
+        unhighlight_tile(selected_tile)
+
+#--------------------------------------------------------------------------------------------------------------
+# Main Game Loop
+#--------------------------------------------------------------------------------------------------------------
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
- 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            process_click_event(pygame.mouse.get_pos())
     # next frame
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
+
+# TODO
+# test move own pieces, replace images
+# create turn based system, check and checkmate
+# two-player on two-processes on local machine, mapping moves between orientations
+# connect machines on separate networks
+# castling, en passant, promotion, audio
+# write project report (networks doc, licensing, state machine, fix projects page) 
---
+++ miscellaneous/board_games/chess/game_pieces.py
@@ -0,0 +1,67 @@
+from abc import abstractmethod
+from enum import Enum
+
+class PieceColor(Enum):
+    WHITE = 0
+    BLACK = 1
+
+class ChessPiece:
+    def __init__(self, x : int, y : str, color : PieceColor):
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getColor(self):
+        return self.color
+
+    def isMoveValid(self, to_coord : (int, int)) -> bool:
+        x, y = to_coord[0], to_coord[1]
+        if not (0 <= x <= 7 and 0 <= y <= 7):
+            raise ValueError("Cannot move piece to tile that does not exist.")
+        return self.inRange(x, y)
+
+    @abstractmethod
+    def inRange(self, x : int, y : int) -> bool:
+        pass
+
+class King(ChessPiece):
+    def inRange(self, x : int, y : int) -> bool:
+        dx = abs(self.x - x)
+        dy = abs(self.y - y)
+        return 0 <= dx <= 1 and 0 <= dy <= 1 and (dx != 0 or dy != 0)
+
+class Queen(ChessPiece):
+    def inRange(self, x : int, y : int) -> bool:
+        dx = abs(self.x - x)
+        dy = abs(self.y - y)
+        return (dx == 0 and dy != 0) or (dy == 0 and dx != 0) or (dx == dy)
+
+class Knight(ChessPiece):
+    def inRange(self, x : int, y : int) -> bool:
+        dx = abs(self.x - x)
+        dy = abs(self.y - y)
+        return (dx == 1 and dy == 2) or (dy == 1 and dx == 2)
+
+class Rook(ChessPiece):
+    def inRange(self, x : int, y : int) -> bool:
+        dx = abs(self.x - x)
+        dy = abs(self.y - y)
+        return (dx == 0 and dy != 0) or (dy == 0 and dx != 0)
+
+class Bishop(ChessPiece):
+    def inRange(self, x : int, y : int) -> bool:
+        dx = abs(self.x - x)
+        dy = abs(self.y - y)
+        return (dx == dy) and (dx != 0 or dy != 0)
+
+class Pawn(ChessPiece):
+    def inRange(self, x : int, y : int) -> bool:
+        dx = abs(self.x - x)
+        dy = abs(self.y - y)
+        return (dx == dy) and (dx != 0 or dy != 0)
